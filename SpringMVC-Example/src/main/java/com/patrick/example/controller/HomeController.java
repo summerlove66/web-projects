d828ea0925f1281798694c382a90dcf09297e943
Thinkpad <summerlove66@163.com>
2018-07-18 01:27:54 +0800

    format code

--- SpringMVC-Example/src/main/java/com/patrick/example/controller/HomeController.java
+++ SpringMVC-Example/src/main/java/com/patrick/example/controller/HomeController.java
@@ -7,12 +7,14 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import javax.validation.Valid;
+
+import com.patrick.example.domain.Student;
+import com.patrick.example.process.StudentMobileEditor;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,68 +22,61 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-@RequestMapping("/")
 @Controller
-public class HomeController{
-	
-	@InitBinder
-	public void initBinder(WebDataBinder binder) {
-		
-		System.out.println("调用 initBinder方法");
-		SimpleDateFormat df = new SimpleDateFormat("yyyy--MM--dd");
-		binder.registerCustomEditor(Date.class,"studentBdo" , new CustomDateEditor(df ,false));
-		binder.registerCustomEditor(Long.class,"studentMobile",new StudentMobileEditor());
-	//	binder.setDisallowedFields(new String[] {"studentMobile","studentAddress.pincode"});
-	}
-	
+public class HomeController {
 
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public String home( @RequestParam(value="username" ,defaultValue="hui") String username, Locale locale, Model model) {
-		
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
 
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		model.addAttribute("username" ,username);
-		
-		return "home";
-	}
-	
-	@RequestMapping("/form")
-	public String submit() {
-	
-		return "simpleform";
-	}
-	
-	@RequestMapping("/except")
-	public void throwException() throws IOException {
-		
-	
-		throw new NullPointerException("you get  the nullpointer  exception");
-	}
-	
-	
-	@RequestMapping("/submit")
-	public String submit(@Valid @ModelAttribute Student student ,BindingResult result) throws UnsupportedEncodingException {
-		
-		
-		if (result.hasErrors()) {
-		System.out.println("result :" +result);
-		return "simpleform";
-		
-		}else {
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("student", student);
-		return "success";
-		
-	}
-	}
+        System.out.println("调用 initBinder方法");
+        SimpleDateFormat df = new SimpleDateFormat("yyyy--MM--dd");
+        binder.registerCustomEditor(Date.class, "studentBdo", new CustomDateEditor(df, false));
+        binder.registerCustomEditor(Long.class, "studentMobile", new StudentMobileEditor());
+        //	binder.setDisallowedFields(new String[] {"studentMobile","studentAddress.pincode"});
+    }
 
-	
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String home(@RequestParam(value = "username", defaultValue = "hui") String username, Locale locale, Model model) {
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+        String formattedDate = dateFormat.format(date);
+        model.addAttribute("serverTime", formattedDate);
+        model.addAttribute("username", username);
+        return "home";
+    }
+
+    @RequestMapping("/form")
+    public String submit() {
+
+        return "simpleform";
+    }
+
+    @RequestMapping("/except")
+    public void throwException() throws IOException {
+
+
+        throw new NullPointerException("you get  the nullpointer  exception");
+    }
+
+
+    @RequestMapping("/submit")
+    public String submit(@Valid @ModelAttribute Student student, BindingResult result) throws UnsupportedEncodingException {
+
+
+        if (result.hasErrors()) {
+            System.out.println("result :" + result);
+            return "simpleform";
+
+        } else {
+            ModelAndView mv = new ModelAndView();
+            mv.addObject("student", student);
+            return "success";
+
+        }
+    }
+
+
 //	@ExceptionHandler(value= IOException.class)
 //	public ModelAndView  handleIOException(Exception e) {
 //		System.out.println("handle the IO exception" +e.getMessage());
@@ -90,18 +85,15 @@ public class HomeController{
 //		mv.addObject("exceptiondesc" ,"错误信息 " +e.getMessage());
 //		return mv;
 //	}
-	
 
-	
 
 //	@ModelAttribute
 //	public void addingCommonObj(Model model) {
 //		System.out.println("首先被调用 1");
 //		model.addAttribute( "headerMessage", "welcome");
 //	}
-	
-	
-	
+
+
 //	@Override
 //	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
 //			throws Exception {	
@@ -109,5 +101,5 @@ public class HomeController{
 //		mv.addObject("serverTime", new Date().toLocaleString() +"----");
 //		return mv;
 //	}
-	
+
 }
